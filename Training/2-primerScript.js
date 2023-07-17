304cdb9da75422311b0c167b4f72f250cf0d18ea
Diriarte01 <danieliriarte01@gmail.com>
2023-07-16 23:30:42 -0500

    update

---
+++ Training/2-primerScript.js
@@ -0,0 +1,28 @@
+/**
+ *@NApiVersion 2.1
+ *@NScriptType UserEventScript
+ *@NModuleScope sameAccount
+ *@Name Mi primer Script
+ *@author Daniel Iriarte
+ *@description Mi primer Script
+ */
+define(['N/record'], function(record) {
+
+    function beforeLoad(context) {
+        
+    }
+
+    function beforeSubmit(context) {
+        
+    }
+
+    function afterSubmit(context) {
+        
+    }
+
+    return {
+        beforeLoad: beforeLoad,
+        beforeSubmit: beforeSubmit,
+        afterSubmit: afterSubmit
+    }
+});
